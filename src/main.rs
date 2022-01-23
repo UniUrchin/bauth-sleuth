ff87e139f4be14f4e760cf1d58383166186281e4
hyperopid <riced1999r@gmail.com>
2022-01-24 00:49:47 +0900

    base64 decode succeed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 use anyhow::Result as AResult;
+use base64::decode;
 use chrono::{DateTime, Local};
 use combine::Parser;
 use combine::parser::char::string;
@@ -8,6 +9,8 @@ use combine::parser::range::{take, take_until_range};
 use etherparse::{InternetSlice, TransportSlice, SlicedPacket};
 use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::Channel::Ethernet;
+
+static TITLE: &str = " - BasicAuth Information - ";
 
 fn main() {
     let network_interface = env::args().nth(1).expect("Error: Cannot find argument!!");
@@ -59,7 +62,35 @@ fn http_parser(timestamp: DateTime<Local>, payload: &[u8]) -> AResult<()> {
     let cleartext = payload.iter().map(|&s| s as char).collect::<String>();
     let ((request_line, _), cleartext) = take_until_range("\r\n").and(take(2)).parse(&*cleartext)?;
     let ((_, uri), _) = string("GET").and(take(1)).and(take_until_range(" ")).parse(request_line)?;
+    let (cleartext, _) = take_until_range("\r\n\r\n").parse(cleartext)?;
+    let request_header = cleartext.split("\r\n").collect::<Vec<&str>>();
 
-    println!("{:?}", uri);
+    let mut request_url = String::new();
+    let mut encrypted_bauth = String::new();
+
+    for content in request_header {
+        let ((header_name, _), value) = take_until_range(":").and(take(2)).parse(content)?;
+
+        match header_name {
+            "Host" => { 
+                request_url = format!("http://{}{}", value, uri);
+            },
+            "Authorization" => {
+                let (_, value) = string("Basic").and(take(1)).parse(value)?;
+                encrypted_bauth = value.to_string();
+            },
+            _ => continue,
+        }
+    }
+
+    let decrypted_bauth = decode(&encrypted_bauth)?.iter().map(|&s| s as char).collect::<String>();
+    let ((username, _), password) = take_until_range(":").and(take(1)).parse(&*decrypted_bauth)?;
+
+    println!("{}\n{}\n{}", "=".repeat(TITLE.len()), TITLE, "=".repeat(TITLE.len()));
+    println!("Timestamp: {}", timestamp.format("%Y/%m/%d %H:%M:%S"));
+    println!("Request_URL: {}", request_url);
+    println!("Username: {}", username);
+    println!("password: {}\n", password);
+
     return Ok(())
 }
